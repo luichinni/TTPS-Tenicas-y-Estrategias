a968d9b26148a0249ced746c1edd664fb0353e1e
Luciano <98102676+luichinni@users.noreply.github.com>
2024-09-22 11:32:23 -0300

    intento de optimizacion (es 200ms mas lento)

--- CD/ej7.py
+++ CD/ej7.py
@@ -4,36 +4,27 @@ lineas = stdin.readlines()
 
 I = 0
 
-while (I < len(lineas) and True):
+mensaje = ''
+
+while(I<len(lineas) and int(lineas[I].replace(' ','')) != 0):
     jack_cant, jill_cant = map(int,lineas[I].split())
     I+=1
 
-    if (jack_cant == 0 and jill_cant == 0):
-        break
-    elif (jack_cant == 0 or jill_cant == 0):
-        print(0)
-        I+=jack_cant
-        I+=jill_cant
-    else:
+    jack_idx = I
+    jill_idx = I + jack_cant
 
-        jack_cd = set()
-        for J in range(jack_cant):
-            nro_cd = int(lineas[I])
-            I+=1
-            jack_cd.add(nro_cd)
-        
-        ultimo = int(lineas[I-1])
+    contador = 0
+    while (jill_idx < (I+jack_cant+jill_cant) and jack_idx < (I+jack_cant)):
+        jack_cd = int(lineas[jack_idx])
+        jill_cd = int(lineas[jill_idx])
+        if (jack_cd == jill_cd):
+            contador+=1
+            jill_idx+=1
+            jack_idx+=1
+        elif (jack_cd > jill_cd):
+            jill_idx+=1
+        elif (jack_cd < jill_cd):
+            jack_idx+=1
 
-        contador = 0
-        J = 0
-        cd_actual = -1
-        while (J < jill_cant and cd_actual < ultimo):
-            cd_actual = int(lineas[I])
-            I+=1
-            if (cd_actual in jack_cd):
-                contador+=1
-            J+=1
-
-        I += (jill_cant - J)
-
-        print(contador)
+    print(contador)
+    I+=jack_cant+jill_cant
